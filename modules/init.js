36afc6217320119e7f6911416b8e44216e0161ce
Purushottam Shukla <formalspace@gmail.com>
2020-01-30 01:33:54 +0530

    [Added] fingerprint module

--- modules/init.js
+++ modules/init.js
@@ -1,8 +1,11 @@
 import Store from "./store.js"
 import Collector from "./collector.js"
 import UserIdentifer from "./userIdentifier.js"
+import Fingerprint from "./fingerprint.js"
 const Init = (function(){
    const _location = Symbol('Location')
+   const _fingerprintId = Symbol('FingerprintId')
+   console.log(Fingerprint)
    return function({userId, secretKey, startOnInit}){
       if(!secretKey) throw "[data-fiber] (init) :secretKey is required parameter in init"
       Store.init({
@@ -10,7 +13,9 @@ const Init = (function(){
          userId: userId || UserIdentifer.getUserId(),
          startOnInit: startOnInit === false ? startOnInit : true,
          sessionDOM: document.getElementsByTagName('html')[0]._fabricDom(),
-         [_location]: window.location
+         [_location]: window.location,
+         [_fingerprintId]: Fingerprint.fingerprintId,
+         deviceInfo: Fingerprint.deviceInfo
       })
       if(startOnInit  === false ? startOnInit : true){
          Collector.start()
